8dec5ff22de17f30f092b93522673a4c6264fa26
Viktor <gurev.va@mail.ru>
2021-07-06 20:36:14 +0300

    test

--- praktikum.java
+++ praktikum.java
@@ -1,9 +1,12 @@
 class praktikum {
     public static void main(String[] arg){
         int a = 0;
-        String s = "asss"
+        int i;
+        String s = "asss";
+
         static int restart(int a) {
            return a + 1;
+
         }
     }
 }
